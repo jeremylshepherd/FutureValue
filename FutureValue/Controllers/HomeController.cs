5e45c9e5684ed9854e9adeb9f923a81a1dad7e7c
Jeremy L. Shepherd <jeremylshepherd@gmail.com>
2021-01-25 18:49:49 -0500

    First Commit

---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FutureValue.Models;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FutureValue = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel fv)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FutureValue = fv.Calculate().ToString("c2");
+            }
+            else{
+                ViewBag.FutureValue = "";
+            }
+
+            return View();
+        }
+    }
+}
